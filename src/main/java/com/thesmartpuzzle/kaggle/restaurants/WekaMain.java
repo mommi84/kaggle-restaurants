202ff87df897e56cafa466fcaddfff8066dbf0b9
Tommaso Soru <mommi84@gmail.com>
2015-04-29 02:26:03 +0200

    Weka data conversion

---
+++ src/main/java/com/thesmartpuzzle/kaggle/restaurants/WekaMain.java
@@ -0,0 +1,50 @@
+package com.thesmartpuzzle.kaggle.restaurants;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+/**
+ * @author Tommaso Soru <tsoru@informatik.uni-leipzig.de>
+ *
+ */
+public class WekaMain {
+
+	public static void main(String[] args) throws IOException {
+
+		run("test");
+
+	}
+
+	private static void run(String prefix) throws IOException {
+
+		final String INPUT_PATH = "data/" + prefix + "-numericdate.csv";
+		final String OUTPUT_PATH = "data/" + prefix + "-numericdate-weka.csv";
+
+		CSVReader reader = new CSVReader(new FileReader(INPUT_PATH), ',', '"',
+				CSVWriter.NO_ESCAPE_CHARACTER);
+		CSVWriter writer = new CSVWriter(new FileWriter(OUTPUT_PATH), ',',
+				CSVWriter.NO_QUOTE_CHARACTER);
+
+		String[] nextLine = reader.readNext(); // column titles
+		String[] writeLine = new String[nextLine.length + 1];
+		for(int i=0; i<nextLine.length; i++)
+			writeLine[i] = nextLine[i];
+		writeLine[writeLine.length - 1] = "revenue";
+		writer.writeNext(writeLine);
+
+		while ((nextLine = reader.readNext()) != null) {
+			for(int i=0; i<nextLine.length; i++)
+				writeLine[i] = nextLine[i];
+			writeLine[writeLine.length - 1] = "0";
+			writer.writeNext(writeLine);
+		}
+
+		writer.close();
+		reader.close();
+	}
+
+}
---
+++ src/main/java/com/thesmartpuzzle/kaggle/restaurants/WekaToKaggle.java
@@ -0,0 +1,48 @@
+package com.thesmartpuzzle.kaggle.restaurants;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+/**
+ * @author Tommaso Soru <tsoru@informatik.uni-leipzig.de>
+ *
+ */
+public class WekaToKaggle {
+
+	public static void main(String[] args) throws IOException {
+
+		run(1);
+
+	}
+
+	private static void run(int n) throws IOException {
+
+		final String INPUT_PATH = "predictions/weka_" + n + ".csv";
+		final String OUTPUT_PATH = "predictions/sent/weka_" + n + ".csv";
+
+		CSVReader reader = new CSVReader(new FileReader(INPUT_PATH), ',', '"',
+				CSVWriter.NO_ESCAPE_CHARACTER);
+		CSVWriter writer = new CSVWriter(new FileWriter(OUTPUT_PATH), ',',
+				CSVWriter.NO_QUOTE_CHARACTER);
+
+		String[] nextLine = reader.readNext(); // column titles
+		String[] writeLine = {"Id", "Prediction"};
+		writer.writeNext(writeLine);
+
+		for (int i=0; (nextLine = reader.readNext()) != null; i++) {
+			if(nextLine.length == 1)
+				break; // don't ask!
+			writeLine[0] = "" + i;
+			writeLine[1] = nextLine[2];
+			writer.writeNext(writeLine);
+		}
+
+		writer.close();
+		reader.close();
+	}
+
+}
